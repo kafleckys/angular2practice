80c92e71fee1c7ce7fb385010325ed7250bd1a34
prakash kafle <kafleprakash0@gmail.com>
2017-08-07 22:26:17 +0545

    Angular 2 starting

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import {TutorialsComponent} from './tutorials.component'
+@Component({
+  selector: 'my-app',
+  template: `<h1>Hello World</h1>
+  			<my-tutorials></my-tutorials>`,
+  directives:[TutorialsComponent]
+})
+export class AppComponent { }
---
+++ app/tutorials.component.ts
@@ -0,0 +1,10 @@
+import{Component} from '@angular/core';
+
+@Component({
+	selector:'my-tutorials',
+	template:'<h1>THis is pratice for angular 2 component making     </h1>'
+})
+
+export class TutorialsComponent{
+	
+}
